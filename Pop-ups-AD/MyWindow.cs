37b7e2e2b6f557cb62212f288f391056fbced417
mhw666 <mhw666@IOTI5R>
2019-03-07 11:27:15 +0800

    提交

--- Pop-ups-AD/MyWindow.cs
+++ Pop-ups-AD/MyWindow.cs
@@ -1,13 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Pop_ups_AD
 {
+    public struct WindowInfo
+    {
+        public IntPtr hWnd;
+        public string szWindowName;
+        public string szClassName;
+    }
     class MyWindow
     {
+        public delegate bool CallBack(int hwnd, int lParam);
+        //枚举窗口
+        [DllImport("user32")]
+        public static extern int EnumWindows(CallBack x, int y);
+
+        //取出标题
+        [DllImport("user32.dll")]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+        [DllImport("user32.dll")]
+        private static extern int GetWindowTextW(IntPtr hWnd, [MarshalAs(UnmanagedType.LPWStr)]StringBuilder lpString, int nMaxCount);
+
+        ////取出类名
+        //[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        //public static extern int GetClassNameA(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+        [DllImport("user32.dll")]
+        public static extern int GetClassNameW(IntPtr hWnd, [MarshalAs(UnmanagedType.LPWStr)]StringBuilder lpString, int nMaxCount);
+        //取出窗口
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        //取出子窗口
+        [DllImport("user32.dll", EntryPoint = "FindWindowEx")]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        //发送消息
+        [DllImport("User32.dll", EntryPoint = "SendMessage", SetLastError = true)]
+        public static extern int SendMessage(IntPtr hWnd, int WM_CHAR, int wParam, int lParam);
 
     }
 }
--- Pop-ups-AD/Program.cs
+++ Pop-ups-AD/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,41 @@ namespace Pop_ups_AD
         static void Main(string[] args)
         {
             //
+            MyWindow.CallBack callBack = new MyWindow.CallBack(Program.Report);
+            MyWindow.EnumWindows(callBack, 0);
+            Console.ReadKey();
+        }
+
+        const int BM_CLICK = 0xF5;
+        private static bool Report(int hwnd, int lParam)
+        {
+            WindowInfo info = GetInfo( new WindowInfo { hWnd = (IntPtr)hwnd });
+            if ("|MSCTFIME UI|Default|Default IME|Mode Indicator|".Contains($"|{info.szWindowName}|")) return true;
+            if ("||".Contains($"|{info.szClassName}|")) return true;
+            if (info.szWindowName != "" && info.szClassName != "")
+            {
+                //Console.WriteLine("{0},n=[{1}]c=[{2}]", hwnd, info.szWindowName, info.szClassName);
+                if (info.szWindowName == "发起会话") {
+                    IntPtr childHwnd = MyWindow.FindWindowEx(info.hWnd, IntPtr.Zero, null, "确定");
+                    MyWindow.SendMessage(childHwnd, BM_CLICK, 0, 0);
+                    MyWindow.SendMessage(childHwnd, BM_CLICK, 0, 0);
+                    Console.WriteLine(DateTime.Now.ToString() + " 成功关闭teamviewer窗口[{0}][{1}][{2}]",
+                        info.hWnd, info.szWindowName, info.szClassName);
+                }
+                //Console.Read();
+            }
+            return true;
+        }
+        
+
+        private static WindowInfo GetInfo(WindowInfo info)
+        {
+            StringBuilder sb = new StringBuilder(256);
+            MyWindow.GetWindowText(info.hWnd, sb, sb.Capacity);
+            info.szWindowName = sb.ToString();
+            MyWindow.GetClassNameW(info.hWnd, sb, sb.Capacity);
+            info.szClassName = sb.ToString();
+            return info;
         }
     }
 }
